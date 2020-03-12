7eb34698888cdc5f33afee20bc2bec974d3067cc
onteriowright <onteriowright@yahoo.com>
2020-03-12 09:58:19 -0500

    first commit

---
+++ Models/DVD.cs
@@ -0,0 +1,9 @@
+namespace GenericIntro
+{
+  public class DVD
+  {
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Genre { get; set; }
+  }
+}
---
+++ Models/Pager.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GenericIntro
+{
+  public class Pager<T>
+  {
+    public int ItemsPerPage { get; set; }
+    public int PageNumber { get; set; }
+    public List<T> AllItems { get; set; }
+    public List<T> GetPage()
+    {
+      return AllItems
+        .Skip(ItemsPerPage * PageNumber)
+        .Take(ItemsPerPage)
+        .ToList();
+    }
+  }
+}
